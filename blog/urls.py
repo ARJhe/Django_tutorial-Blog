2431ec4e0f1238c311e286c90e12131eb1f86998
ARJhe <meopkids@gmail.com>
2020-01-24 08:04:00 +0800

    django tutorial

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    # get views.py home() function
+    path('', views.home, name='blog_home'),
+    path('about/', views.about, name='blog_about'),
+
+]
---
+++ django_project/urls.py
@@ -0,0 +1,25 @@
+"""django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    # path method first parameter attach to main url
+    # if it is '', we can set it as default home page
+    path('admin/', admin.site.urls),
+    # direct urls to blog app's urls.py
+    path('', include('blog.urls')),
+]
