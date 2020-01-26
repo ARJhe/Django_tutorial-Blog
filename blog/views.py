5c5e7592e1d936656ed0e4851bcc7790c1e1d412
ARJhe <meopkids@gmail.com>
2020-01-26 10:57:48 +0800

    1. Using PostgreSQL with Django #1 2. add new model Post #2 3. substitute Post's records for default dictionary.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [{
-    'author':'JHE',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'Jan 23, 2020'
-    },
-    {'author':'JHE',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'Jan 23, 2020'
-    },
-]
+# posts = [{
+#     'author': 'JHE',
+#     'title': 'Blog Post 1',
+#     'content': 'First post content',
+#     'date_posted': 'Jan 23, 2020'
+# },
+#     {'author': 'JHE',
+#      'title': 'Blog Post 2',
+#      'content': 'Second post content',
+#      'date_posted': 'Jan 23, 2020'
+#      },
+# ]
+
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': "About"})
